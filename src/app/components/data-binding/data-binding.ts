1a730bd9b6e6a1f303ece258add06caaa9f1e04e
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-08-16 11:25:13 -0300

    Angular 22 Tutorial for Beginners | Data Binding and Event Binding in Angular

--- src/app/components/data-binding/data-binding.ts
+++ src/app/components/data-binding/data-binding.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css',
 })
-export class DataBinding {}
+export class DataBinding {
+  productName: string = 'Headphone';
+  productPrice: number = 1200;
+  maxAllowedQuantity: number = 5;
+  isProductActive = false;
+
+  myDynamicType = 'button';
+
+  showWelcomeText() {
+    alert('Bem vindo v22');
+  }
+
+  onDropdownChange() {
+    alert('Dropdown has changed');
+  }
+
+  onMouseEnter() {
+    console.log('Mouse Enytered');
+  }
+
+  onMouseLeft() {
+    console.log('Mouse Left Div');
+  }
+}
