82ea0b80b0b0145ed93b76a93ecc36ae1d122678
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-08-18 22:27:39 -0300

    Angular 22 Tutorial for Beginners | Directive in Angular | ngClass & ngStyle

--- src/app/components/directive-exp/directive-exp.ts
+++ src/app/components/directive-exp/directive-exp.ts
@@ -1,9 +1,53 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directive-exp',
-  imports: [],
+  imports: [NgClass, NgStyle, FormsModule],
   templateUrl: './directive-exp.html',
   styleUrl: './directive-exp.css',
 })
-export class DirectiveExp {}
+export class DirectiveExp {
+  divClassName: string = 'bg-success';
+
+  isCheckActive = false;
+
+  num1 = '';
+
+  isTextValid = false;
+
+  inputColorValue = '';
+
+  div4Css: any = {};
+
+  addDiv1Class(className: string) {
+    this.divClassName = className;
+  }
+
+  checkCharLength() {
+    debugger;
+    if (this.num1.length > 5) {
+      this.isTextValid = true;
+    } else {
+      this.isTextValid = false;
+    }
+  }
+
+  changeType(type: string) {
+    if (type == 'circle') {
+      this.div4Css = {
+        'background-color': 'red',
+        height: '200px',
+        width: '200px',
+        'border-radius': '50%',
+      };
+    } else if (type == 'square') {
+      this.div4Css = {
+        'background-color': 'blue',
+        height: '200px',
+        width: '200px',
+      };
+    }
+  }
+}
